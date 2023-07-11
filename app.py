e879812a8793e39442012f09fdc6f2fe362c8b6d
standemdem <stan.demontmarin@gmail.com>
2023-07-11 12:06:13 +0200

    created project structure

---
+++ app.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect("test.db")
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS 
+    students (
+        id INTEGER PRIMARY KEY,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        age INTEGER,
+        grade FLOAT
+    );
+""")
